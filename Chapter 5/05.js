b072bbe434919c9cb31c5f321399943795dbdb88
hwy1782 <hwy1782@gmail.com>
2016-06-16 18:09:58 +0800

    引用添加分段参数

--- Chapter 5/05.js
+++ Chapter 5/05.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     $("<a href='#top'>back to top</a>").insertAfter("div.chapter p");
     $("<a id='top'></a>").prependTo("body");
 
-    $("span.footnote").insertBefore("#footer");
+    $("span.footnote").insertBefore("#footer").wrapAll("<ol id='notes'></ol>").wrap("<li></li>");
 
     $("div.chapter a[href*='wikipedia']").attr({
             rel: 'external',
