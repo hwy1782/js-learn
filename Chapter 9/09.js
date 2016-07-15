a5c2c7037f02b13efa3fd266e98882351f58e4a0
hwy1782 <hwy1782@gmail.com>
2016-07-15 17:17:46 +0800

    topic点击时添加样式

--- Chapter 9/09.js
+++ Chapter 9/09.js
@@ -4,3 +4,13 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+$(document).ready(function () {
+    $("#topics a").click(function (event) {
+        // 阻止连接打开新的页面
+        event.preventDefault();
+        // 删除所有topic上的selected样式
+        $("#topics a.selected").removeClass("selected");
+        // 在被点击的selected上添加样式
+        $(this).addClass("selected");
+    });
+});
